49b6487a1e28f33f52d7dc85d9bb428b69f007aa
Kasejohnson01 <KaseJohnson01@gmail.com>
2025-02-11 23:48:53 -0600

    Socials added, css added for responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero (){
     return (
         <div className="hero-container">
             <h1 className="hero-title">Hi, I'm Kase Johnson</h1>
-            <h3 className="hero-desc">Welcome to my Portfolio Website</h3>
+            <h3 className="hero-desc">Welcome to my Portfolio</h3>
         </div>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/pages/home.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 
 
@@ -8,7 +10,17 @@ function Home() {
 
     return (
         <div className = "home-container">
-            Test
+            <div className="socials">
+                <p className="socials-title">Socials</p>
+                <div className="icons-container">
+                    <a href="https://github.com/Kasej01" target="_blank" rel="noopener noreferrer" className="icon-a" id="github-icon">
+                        <FontAwesomeIcon icon={faGithub} className="icon" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/kase-johnson-02a974205/" target="_blank" rel="noopener noreferrer" className="icon-a" id="linkedin-icon">
+                        <FontAwesomeIcon icon={faLinkedin} className="icon" />
+                    </a>
+                </div>
+            </div>
         </div>
     );
 }
